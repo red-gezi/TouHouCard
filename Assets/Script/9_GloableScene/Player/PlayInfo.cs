6ad0f45820fd091c0d2348a9839ff2d4dc01fed0
red-gezi <506065448@qq.com>
2019-03-12 17:13:03 +0800

    3.12增量更新
    调整脚本位置

--- Assets/Script/9_GloableScene/Player/PlayInfo.cs
+++ Assets/Script/9_GloableScene/Player/PlayInfo.cs
@@ -8,16 +8,10 @@ namespace Info
 {
     public class PlayInfo : SerializedMonoBehaviour
     {
-        //public static PlayInfo Instance;
-        //string Name = "gezi";
         [ShowInInspector]
         public static PlayerInfo MyInfo;
         [ShowInInspector]
         public static PlayerInfo OpInfo;
-        //private void Awake()
-        //{
-        //    Instance = this;
-        //}
     }
 }
 
